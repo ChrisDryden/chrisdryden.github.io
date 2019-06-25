ca6b55d0a92cb31c362adbebeb7d5c1c2a2545b5
Chris Dryden <christopher.paul.dryden@gmail.com>
2019-06-25 01:00:09 -0400

    Removed additional directories

--- quote/js/data.js
+++
@@ -1,8 +0,0 @@
-const quotes = [
-    "There is no God but Allah and Muhammad is the Messenger of Allah.",
-    "We are nearer to him than his jugular vein.",
-    "Indeed, Allah forgives All Sins.",
-    "Suffering is a gift. In it is hidden mercy.",
-    "Islam is perfect, there is nothing to be added or changed.",
-    "I saw Islam as the correct way to live, and I chose to try to live that way."
-];
